7e3f557a137b3a546a52ceb4084eda66d54f7504
GokuMohandas <gokumd@gmail.com>
2021-10-01 21:49:42 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,68 @@
+# setup.py
+# Setup installation for the application
+
+from pathlib import Path
+
+from setuptools import find_namespace_packages, setup
+
+BASE_DIR = Path(__file__).parent
+
+# Load packages from requirements.txt
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+test_packages = [
+    "great-expectations==0.13.14",
+    "pytest==6.0.2",
+    "pytest-cov==2.10.1",
+]
+
+dev_packages = [
+    "black==20.8b1",
+    "flake8==3.8.3",
+    "isort==5.5.3",
+    "jupyterlab==2.2.8",
+    "pre-commit==2.11.1",
+]
+
+docs_packages = [
+    "mkdocs==1.1.2",
+    "mkdocs-macros-plugin==0.5.0",
+    "mkdocs-material==6.2.4",
+    "mkdocstrings==0.14.0",
+]
+
+setup(
+    name="tagifai",
+    version="0.1",
+    license="MIT",
+    description="Tag suggestions for projects on Made With ML.",
+    author="Goku Mohandas",
+    author_email="hello@madewithml.com",
+    url="https://madewithml.com/",
+    keywords=[
+        "machine-learning",
+        "artificial-intelligence",
+        "madewithml",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires="==3.7.10",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+    extras_require={
+        "test": test_packages,
+        "dev": test_packages + dev_packages + docs_packages,
+        "docs": docs_packages,
+    },
+    entry_points={
+        "console_scripts": [
+            "tagifai = tagifai.main:app",
+        ],
+    },
+)
