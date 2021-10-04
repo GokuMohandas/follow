ceb11d4c79bda0c741fd05464aa3ae8e9ca35a60
GokuMohandas <gokumd@gmail.com>
2021-10-04 11:36:08 -0700

    .

--- config/config.py
+++ config/config.py
@@ -21,16 +21,12 @@ DATA_DIR = Path(BASE_DIR, "data")
 STORES_DIR = Path(BASE_DIR, "stores")
 
 # Local stores
-BLOB_STORE = Path(STORES_DIR, "blob")
-FEATURE_STORE = Path(STORES_DIR, "feature")
 MODEL_REGISTRY = Path(STORES_DIR, "model")
 
 # Create dirs
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 STORES_DIR.mkdir(parents=True, exist_ok=True)
-BLOB_STORE.mkdir(parents=True, exist_ok=True)
-FEATURE_STORE.mkdir(parents=True, exist_ok=True)
 MODEL_REGISTRY.mkdir(parents=True, exist_ok=True)
 
 # MLFlow model registry
